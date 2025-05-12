5d3cd37a58102ea498ea4a36c604adac84cc2753
adAstra-00 <135280560+adAstra-00@users.noreply.github.com>
2025-05-12 12:18:22 +0100

    sidebar + icons + animations

---
+++ writing-hub/app/(main)/(routes)/documents/page.tsx
@@ -0,0 +1,10 @@
+const DocumentsPage = () => {
+    return (
+        <div>
+            This is a protected DocumentsPage
+            
+        </div>
+     );
+}
+ 
+export default DocumentsPage;
---
+++ writing-hub/app/(main)/layout.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { useConvexAuth } from "convex/react";
+import { redirect } from "next/navigation";
+
+import { Spinner } from "@/components/spinner";
+
+import { Navigation } from "./_components/navigation";
+
+const MainLayout = ({ //"Let's extract the children"
+    children
+}: { //"Let's give the children a type of ..."
+    children: React.ReactNode
+}) => { //add some hooks here
+    // since logic is inside of layout file and not specific page every route inside of our (main) app is now auth protected
+    const { isAuthenticated, isLoading } = useConvexAuth();
+
+    if (isLoading) {
+        return (
+            <div className="h-full flex items-center justify-center">
+                <Spinner size="lg" />
+            </div>
+        )
+    }
+    if (!isAuthenticated) {
+        return redirect("/");
+    }
+
+    return (
+        <div className="h-full flex dark:bg-[#1F1F1F]">
+            <Navigation />
+            <main className="flex-1 h-full overflow-y-auto">
+                {children} {/* "let's render the children" */}
+            </main>
+        </div>
+    );
+}
+
+export default MainLayout;
