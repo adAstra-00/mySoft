1e38b33e536006fef2168f683fbf11f0c45629af
adAstra-00 <135280560+adAstra-00@users.noreply.github.com>
2025-05-19 11:10:41 +0100

    feat: add search functionality and new components, update dependencies

--- writing-hub/app/(main)/_components/navigation.tsx
+++ writing-hub/app/(main)/_components/navigation.tsx
@@ -11,6 +11,7 @@ import {
     PopoverTrigger,
     PopoverContent,
 } from "@/components/ui/popover";
+import { useSearch } from "@/hooks/use-search";
 
 import { toast } from "sonner";
 import { Item } from "./item";
@@ -19,6 +20,7 @@ import { DocumentList } from "./document-list";
 import { TrashBox } from "./trash-box";
 
 export const Navigation = () => {
+    const search =useSearch();
     const pathname = usePathname();
     const isMobile = useMediaQuery("(max-width:768px)");
 
@@ -134,7 +136,7 @@ export const Navigation = () => {
                         label="Search"
                         icon={Search}
                         isSearch
-                        onClick={() => { }}
+                        onClick={search.onOpen}
                     />
                     <Item
                         label="Settings"
--- writing-hub/app/(main)/layout.tsx
+++ writing-hub/app/(main)/layout.tsx
@@ -6,6 +6,7 @@ import { redirect } from "next/navigation";
 import { Spinner } from "@/components/spinner";
 
 import { Navigation } from "./_components/navigation";
+import { SearchCommand } from "@/components/search-command";
 
 const MainLayout = ({ //"Let's extract the children"
     children
@@ -24,12 +25,13 @@ const MainLayout = ({ //"Let's extract the children"
     }
     if (!isAuthenticated) {
         return redirect("/");
-    }
+    } 
 
     return (
         <div className="h-full flex dark:bg-[#1F1F1F]">
             <Navigation />
             <main className="flex-1 h-full overflow-y-auto">
+                <SearchCommand />
                 {children} {/* "let's render the children" */}
             </main>
         </div>
